7a8b9c5987cb7b7c055a2e23089290cfd39519d9
LyeZinho <pedrokalebdej1@gmail.com>
2025-06-20 11:53:43 +0100

    Initial commit

---
+++ src/src/Form1.cs
@@ -0,0 +1,31 @@
+using Microsoft.Web.WebView2.WinForms;
+
+namespace src
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the WebView2 control on: https://devscafe.org/radio
+            webView.Source = new Uri("https://devscafe.org/radio");
+
+            // Resize with the window
+            webView.Dock = DockStyle.Fill;
+
+            // Enable the WebView2 control
+            webView.CoreWebView2InitializationCompleted += (sender, e) =>
+            {
+                if (e.IsSuccess)
+                {
+                    Console.WriteLine("WebView2 initialized successfully.");
+                }
+                else
+                {
+                    Console.WriteLine($"WebView2 initialization failed: {e.InitializationException}");
+                }
+            };
+        }
+    }
+}
